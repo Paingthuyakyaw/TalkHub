5c3257f41f0b07cc41eb305622030331da235f84
Paingthuyakyaw <kyawna2265@gmail.com>
2025-12-06 00:36:15 +0630

    feat: protected route

--- server/index.js
+++ server/index.js
@@ -6,10 +6,18 @@ import { verifyToken } from "./middleware/verify-token.js";
 import messageRouter from "./router/message.route.js";
 import userRouter from "./router/user.route.js";
 import { app, server } from "./socket/message.socket.js";
+import cors from "cors";
+
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*", // frontend URL
+    credentials: true, // cookies or auth headers allow
+  })
+);
 
 app.get("/", (req, res) => res.status(200).json({ message: "HELLO GUY" }));
 app.get("/api/me", verifyToken, (req, res) =>
